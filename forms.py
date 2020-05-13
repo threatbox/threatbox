4974fbc8753ab2a481423f707b782d7f533ba450
threatbox <62971254+threatbox@users.noreply.github.com>
2020-05-13 22:05:22 +0200

    Add files via upload

--- forms.py
+++ forms.py
@@ -153,6 +153,7 @@ class FuenteForm(Form):
     fiabilidad = RadioField(
         'Score de fiabilidad',
         choices=(
+            ('0', '0'),
             ('1', '1'),
             ('2', '2'),
             ('3', '3'),
--- lanzador.py
+++ lanzador.py
@@ -3,8 +3,9 @@ import os
 import bcrypt
 from bson.objectid import ObjectId
 from pymongo import MongoClient
-from flask import Flask, render_template, request, session, flash, redirect, \
+from flask import Flask, render_template, request, session, redirect, \
     url_for
+from flask_paginate import Pagination, get_page_args
 
 import forms
 
@@ -13,52 +14,49 @@ app = Flask(__name__)
 app.secret_key = 'clave super secreta de encriptación de sesión'
 app.config['SESSION_TYPE'] = 'filesystem'
 
-
 client = MongoClient(os.environ.get('MONGODB'))
 db = client.threatbox
 db_usr = db.usuarios
 db_fee = db.fuentes
 feeds = db.threatbox['feeds']
 
-SEARCH_LIMIT = 100  # número máximo de resultados en las búsquedas
+SEARCH_LIMIT = 1000  # número máximo de resultados en las búsquedas
 
 
 @app.route('/')
 @app.route('/buscar')
 def buscar():
+    page, per_page, offset = get_page_args(
+        page_parameter='page', per_page_parameter='per_page'
+    )
     datos = db_fee.find()
-    # print(datos)
     busca = request.args.get('busca', '')
     if busca:
         datos = db_fee.find({
             "$or": [
-                {'categoria': {'$regex': busca}},
-                {'subcategoria': {'$regex': busca}},
-                {'fuente': {'$regex': busca}},
-                {'ubicacion': {'$regex': busca}},
-                {'direccion': {'$regex': busca}},
-                {'darknet': {'$regex': busca}},
-                {'tipo': {'$regex': busca}},
-                {'idioma': {'$regex': busca}},
+                {'categoria': {'$regex': busca, '$options': 'i'}},
+                {'subcategoria': {'$regex': busca, '$options': 'i'}},
+                {'fuente': {'$regex': busca, '$options': 'i'}},
+                {'ubicacion': {'$regex': busca, '$options': 'i'}},
+                {'direccion': {'$regex': busca, '$options': 'i'}},
+                {'darknet': {'$regex': busca, '$options': 'i'}},
+                {'tipo': {'$regex': busca, '$options': 'i'}},
+                {'idioma': {'$regex': busca, '$options': 'i'}},
                 # {'nivel': {'$regex': busca}},
                 # {'invitacion': {'$regex': busca}},
                 # {'fiabilidad': {'$regex': busca}}
             ]
         })
-        
-    datos = list(datos)
-    return render_template('buscar.html', datos=datos, busca=busca)
+        # datos = list(datos)
+        pagination = Pagination(page=page, total=datos.count(), busca=busca)
+        datos = list(datos.skip(offset).limit(per_page))
+    else:
+        datos = []
+        pagination = Pagination(page=page, total=0, busca=busca)
 
-
-@app.route('/search', methods=['POST', 'GET'])
-def search():
-    busqueda = request.form['busca']
-    resultado_busqueda = db.find(
-        'fuente', search=busqueda, filter={'related':True}, limit=SEARCH_LIMIT
+    return render_template(
+        'buscar.html', datos=datos, busca=busca, pagination=pagination, total=len(datos)
     )
-    resultados = (res['obj'] for res in resultado_busqueda['results'])
-    return render_template('search.html', results=resultados)
-
 
 
 @app.route('/login', methods=['POST', 'GET'])
@@ -66,12 +64,14 @@ def login():
     form = forms.LoginForm(request.form)
     if request.method == 'POST' and form.validate():
         login_user = db_usr.find_one({
-            'name' : form.username.data
+            'name': form.username.data
         })
-        print(login_user)
         if login_user:
-            if bcrypt.checkpw(form.password.data.encode('utf-8'), login_user['password']):
-                return redirect(url_for('user_up'))
+            if bcrypt.checkpw(
+                form.password.data.encode('utf-8'), login_user['password']
+            ):
+                session['name'] = form.username.data
+                return redirect(url_for('upload'))
             else:
                 form.password.errors.append('La contraseña no coincide')
         else:
@@ -80,42 +80,45 @@ def login():
     return render_template('login.html', form=form)
 
 
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('buscar'))
+
+
 @app.route('/register', methods=['POST', 'GET'])
 def register():
     form = forms.RegistroForm(request.form)
     if request.method == 'POST' and form.validate():
-        existing_user = db_usr.find_one({'name' : form.username.data})
-        # print(existing_user)
+        existing_user = db_usr.find_one({'name': form.username.data})
         if existing_user is None:
             hashpass = bcrypt.hashpw(
-                form.username.data.encode('utf-8'), bcrypt.gensalt()
+                form.password.data.encode('utf-8'), bcrypt.gensalt()
             )
             db_usr.insert({
-                'name' : form.username.data, 
-                'password' : hashpass, 
-                'email' : form.email.data
+                'name': form.username.data,
+                'password': hashpass,
+                'email': form.email.data
             })
             session['name'] = form.username.data
-            return redirect(url_for('login'))
+            return redirect(url_for('user_up'))
         else:
             form.username.errors.append('El usuario ya existe')
 
     return render_template('register.html', form=form)
 
 
-@app.route('/user_up')
-def user_up():
-    return render_template('bienvenida.html')
-
-
 @app.route('/upload', methods=['POST', 'GET'])
 def upload():
-    form = forms.FuenteForm(request.form)
-    if request.method == 'POST' and form.validate():
-        pk = db_fee.insert_one(form.data).inserted_id
-        return redirect(url_for('ficha', id=pk))
+    if 'name' in session:
+        form = forms.FuenteForm(request.form)
+        if request.method == 'POST' and form.validate():
+            pk = db_fee.insert_one(form.data).inserted_id
+            return redirect(url_for('ficha', id=pk))
 
-    return render_template('upload.html', form=form)
+        return render_template('upload.html', form=form)
+    else:
+        return redirect(url_for('login'))
 
 
 # https://pymongo.readthedocs.io/en/stable/tutorial.html
@@ -123,8 +126,6 @@ def upload():
 @app.route('/detalle/<id>')
 def ficha(id):
     fee = db_fee.find_one({'_id': ObjectId(id)})
-    print(id)
-    print(list(fee))
     return render_template('ficha.html', fee=fee)
 
 
